6c73eeaae0445aa5bc2602d7b78dd3b91a19a3dd
Shijun Cheng <112085870+sunnyshijuncheng@users.noreply.github.com>
2024-11-24 10:45:43 +0300

    Update sample_patchfusion.py

--- diff_image_enhancement/sample_patchfusion.py
+++ diff_image_enhancement/sample_patchfusion.py
@@ -1,8 +1,3 @@
-"""
-Generate a large batch of image samples from a model and save them as a large
-numpy array. This can be used to produce samples for FID evaluation.
-"""
-
 import argparse
 import os
 
